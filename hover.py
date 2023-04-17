6435f38df05f3828a82c0dbdbde90874351e1a79
endaaman <ken@endaaman.me>
2023-04-17 15:24:16 +0900

    update

--- hover.py
+++ hover.py
@@ -27,26 +27,29 @@ def get_color_map(alpha=255):
 
 class CLI(BaseCLI):
     class CommonArgs(define_ml_args(seed=42)):
-        hoge: int = 32
-
-    def pre_common(self, a:CommonArgs):
-        self.model = HoVerNetExt(num_types=7, pretrained_backbone='weights/resnet50-0676ba61.pth')
-        # weight = torch.load('weights/hovernet-conic.pth', map_location=lambda storage, loc: storage)
-        weight = torch.load('weights/hovernet-conic.pth', map_location=torch.device('cpu'))
-        self.model.load_state_dict(weight)
-        self.model.eval()
-
-    class PredArgs(define_ml_args(seed=42)):
         src: str
         dest: str = 'out'
 
-    def run_pred(self, a):
+    class HoverArgs(define_ml_args(seed=42)):
+        pass
+
+    def init_hover_net(self):
+        model = HoVerNetExt(num_types=7, pretrained_backbone='weights/resnet50-0676ba61.pth')
+        # weight = torch.load('weights/hovernet-conic.pth', map_location=lambda storage, loc: storage)
+        weight = torch.load('weights/hovernet-conic.pth', map_location=torch.device('cpu'))
+        model.load_state_dict(weight)
+        model.eval()
+        return model
+
+    def run_hover(self, a):
+        model = self.init_hover_net()
+
         ii, pp = load_images_from_dir_or_file(a.src, with_path=True)
         original_img, original_path = ii[0], pp[0]
         original_name = os.path.splitext(os.path.basename(original_path))[0]
 
         t = torch.from_numpy(np.array(original_img.convert('RGB')))[None, ...].permute(0, 3, 1, 2)
-        o = self.model(t)
+        o = model(t)
         m = o['tp'][0].detach().numpy()
         m = np.argmax(m, axis=0)
 
@@ -55,7 +58,6 @@ class CLI(BaseCLI):
 
         overlay_img = original_img.convert('RGBA').copy()
         overlay_img.paste(mask_img, (0, 0), mask_img)
-
 
         original_img.save(J(a.dest, f'{original_name}_original.png'))
         mask_img.save(J(a.dest, f'{original_name}_mask.png'))
--- sd.py
+++ sd.py
@@ -1,8 +1,8 @@
 import os
 from types import SimpleNamespace
 
-import numpy as np
 from csbdeep.utils.tf import limit_gpu_memory
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -11,10 +11,10 @@ from stardist.models import Config2D, StarDist2D
 
 from conic import get_data, oversample_classes, CLASS_NAMES
 from conic import HEStaining, HueBrightnessSaturation
+
 from augmend import (
     Augmend,
     AdditiveNoise,
-    Augmend,
     Elastic,
     FlipRot90,
     GaussianBlur,
@@ -32,5 +32,36 @@ def get_class_count(Y0):
     df = pd.DataFrame(class_count, index=CLASS_NAMES.values(), columns=["counts"])
     df = df.drop("BACKGROUND")
     df["%"] = (100 * (df["counts"] / df["counts"].sum())).round(2)
-    display(df)
+    print(df)
     return class_count
+
+args = SimpleNamespace()
+
+# data in
+args.datadir     = "./datasets/CoNIC" # path to 'Patch-level Lizard Dataset' as provided by CoNIC organizers
+args.oversample  = True     # oversample training patches with rare classes
+args.frac_val    = 0.1      # fraction of data used for validation during training
+args.seed        = None     # for reproducible train/val data sets
+
+# model out (parameters as used for our challenge submissions)
+args.modeldir    = "./models"
+args.epochs      = 1000
+args.batchsize   = 4
+args.n_depth     = 4
+args.lr          = 3e-4
+args.patch       = 256
+args.n_rays      = 64
+args.grid        = (1,1)
+args.head_blocks = 2
+args.augment     = True
+args.cls_weights = False
+
+args.workers     = 1
+args.gpu_datagen = False and args.workers==1 and gputools_available() # note: ignore potential scikit-tensor error
+
+print(vars(args))
+
+
+X, Y, D, Y0, idx = get_data(args.datadir, seed=args.seed)
+X, Xv, Y, Yv, D, Dv, Y0, Y0v, idx, idxv = train_test_split(X, Y, D, Y0, idx, test_size=args.frac_val, random_state=args.seed)
+class_count = get_class_count(Y0)
